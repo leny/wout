cf292348d24e43e47e1f43c026dabe4929e627b1
Leny <lisarael@gmail.com>
2012-05-12 13:09:42 +0200

    Suppression de re?fe?rences fanto?mes

--- src/wout.php
+++ src/wout.php
@@ -9,11 +9,6 @@ const WOUT_VERSION = '0.1';
 
 include( __DIR__ . '/tools/singleton.php' );
 
-include( __DIR__ . '/classes/utils/utils.php' );
-include( __DIR__ . '/classes/utils/convertor.php' );
-include( __DIR__ . '/classes/utils/globals.php' );
-include( __DIR__ . '/classes/utils/void.php' );
-
 include( __DIR__ . '/classes/routing/router.php' );
 include( __DIR__ . '/classes/routing/route.php' );
 include( __DIR__ . '/classes/routing/error_route.php' );
